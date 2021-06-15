48451c97079452bc782b3581c6b28f35e05fcc86
eugabiviana <gabi.viana88@gmail.com>
2021-06-15 10:15:34 -0300

    Correction of DetalhesDoAutorResponse

--- nossa-casa-do-codigo/src/main/kotlin/br/com/zup/autores/DetalhesDoAutorResponse.kt
+++ nossa-casa-do-codigo/src/main/kotlin/br/com/zup/autores/DetalhesDoAutorResponse.kt
@@ -1,8 +1,8 @@
 package br.com.zup.autores
 
-class DetalhesDoAutorResponse(autor: Autor) {
+class DetalhesDoAutorResponse(   val nome: String,
+                                 val email: String,
+                                 val descricao: String) {
 
-    val nome = autor.nome
-    val email = autor.email
-    val descricao = autor.descricao
+    constructor(autor: Autor) : this(autor.nome, autor.email, autor.descricao)
 }
